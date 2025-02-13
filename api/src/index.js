57d7c6fa418d8caec1380c51990c4dd4ed6777ee
Martiniano Gomes <martinianogomes@unifesspa.edu.br>
2025-02-13 20:56:35 -0300

    refactor: Alterando configurações de servidor.

--- api/src/index.js
+++ api/src/index.js
@@ -1,22 +1,3 @@
-// Importação das dependências do sistema, como o Express e Mongoose.
-// O Express é para o auxílio do manuseio do servidor do sistema.
-// O Mongoose é para o manuseio do banco de dados do sistema.
-// o Swagger é para documentação da API.
-import express from 'express'
-import mongoose from 'mongoose'
-
 import router from './routes/routes.js'
 
-// Iniciando as instâncias do Express e dizendo para ele trabalhar com JSON's.
-const app = express()
-app.use(express.json())
-
-const port = 3000
-
 app.use(router)
-
-app.listen(port, (req, res) => {
-    mongoose.connect('mongodb://localhost:27017')
-
-    console.log('O programa está escutando na porta: ' + port)
-})
