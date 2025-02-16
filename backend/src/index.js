b48f8d92461a77911b5dde7a7ae0faf2fbfb52b3
Martiniano Gomes <martinianogomes@unifesspa.edu.br>
2025-02-16 18:31:27 -0300

    refactor: Alterando a organização do projeto.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,22 +1,37 @@
-import express from 'express';
-import router from './routes/routes.js';
-import './config/database.js';
+// import express from 'express';
+// import router from './routes/routes.js';
+// import './config/database.js';
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(router);
+
+// const startServer = async () => {
+//     try {
+//         await connect();
+//         console.log('Conexão com o banco de dados estabelecida com sucesso.');
+//         app.listen(3000, () => {
+//             console.log('Servidor rodando na porta 3000.');
+//         });
+//     } catch (error) {
+//         console.error('Erro ao iniciar o servidor:', error);
+//     }
+// };
+
+// startServer();
+
+const express = require('express');
+const bookRoutes = require('./routes/routesPaths/bookRoutes');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(router);
+app.use(express.urlencoded({ extended: true }));
 
-const startServer = async () => {
-    try {
-        await connect();
-        console.log('Conexão com o banco de dados estabelecida com sucesso.');
-        app.listen(3000, () => {
-            console.log('Servidor rodando na porta 3000.');
-        });
-    } catch (error) {
-        console.error('Erro ao iniciar o servidor:', error);
-    }
-};
+app.use('/books', bookRoutes);
 
-startServer();
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
